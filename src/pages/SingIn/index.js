549b438c9a34ffb41d0cc4ea5997895224bff4ad
Ricardo Junior <ricardojunior199202@gmail.com>
2019-11-01 11:27:42 -0300

    Front ReactJS

---
+++ src/pages/SingIn/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
+import * as Yup from "yup";
+
+import { singInRequest } from "~/store/modules/auth/actions";
+
+import logo from "~/assets/logo.svg";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Insira um e-mail valido")
+    .required("O e-mail e obrigatorio"),
+  password: Yup.string().required("Senha obrigatoria")
+});
+
+export default function SingIn() {
+  const dispath = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispath(singInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">{loading ? "Carregando..." : "Acessar"}</button>
+        <Link to="/register">Criar conta</Link>
+      </Form>
+    </>
+  );
+}
